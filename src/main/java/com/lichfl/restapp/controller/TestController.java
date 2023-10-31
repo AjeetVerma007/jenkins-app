3f582908342ef562c23a490351cc7e80722787ed
Ajeet Verma <ajeet57@gmail.com>
2023-10-31 07:55:01 +0530

    Second commit

--- src/main/java/com/lichfl/restapp/controller/TestController.java
+++ src/main/java/com/lichfl/restapp/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.lichfl.restapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
+
 	@GetMapping("/display")
 	public String display() {
+
+		logger.info("display");
 		return "Hi welome to jenkins";
 	}
 
